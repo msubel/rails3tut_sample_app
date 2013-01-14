43224159320a92b50f1ee5f7ef6b7a0478db291c
Michal Subel <michal.subel@me.com>
2013-01-14 01:00:39 +0100

    rspec refactor with let

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,15 +3,17 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
-    @title = 'Ruby on Rails Tutorial Sample App'
+   
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
      	page.should have_selector('h1', :text => 'Sample App')
     end
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "#{@title} | Home")
+    	page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
   
@@ -22,7 +24,7 @@ describe "StaticPages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => "#{@title} | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
   end
   
@@ -33,7 +35,7 @@ describe "StaticPages" do
   	end
   	it "should have the title 'About Us'" do
   		visit '/static_pages/about'
-  		page.should have_selector('title', :text => "#{@title} | About Us")
+  		page.should have_selector('title', :text => "#{base_title} | About Us")
   	end
   end
 
