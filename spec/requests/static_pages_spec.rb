005c73d11e888fd978226276b21f0a89337761ac
Michal Subel <michal.subel@me.com>
2013-01-15 23:14:01 +0100

    Changed get to match in routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
    
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
      	page.should have_selector('h1', :text => 'Sample App')
     end
    
     it "should have the base title 'Home'" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should have_selector('title', :text => base_title)
     end
   
   	it "should not have a custom page title" do
-  		visit '/static_pages/home'
+  		visit root_path
   		page.should_not have_selector('title', :text => "| Home")
   	end
   
@@ -26,23 +26,34 @@ describe "StaticPages" do
   
   describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
   end
   
   describe "About page" do
   	it "should have the content 'About'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About')
   	end
   	it "should have the title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title', :text => "#{base_title} | About Us")
+  	end
+  end
+  
+  describe "Contact page" do
+		it "shuold have the h1 'Contant'" do
+			visit contact_path
+			page.should have_selector('h1', text: 'Contact')
+		end
+  	it "should have the title 'Contact'" do
+  		visit contact_path
+  		page.should have_selector('title', :text => "#{base_title} | Contact")
   	end
   end
 
